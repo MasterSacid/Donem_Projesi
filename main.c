796ef03789af7a4bf6385f97e4013069261838c0
Sacit <sacidaktan1@hotmail.com>
2024-12-11 22:59:30 +0300

    sayi girme oyunu

--- main.c
+++ main.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+int toplam = 0;
+int sayiTopla(int x);
 int main(void) {
-    printf("Hello, World!\n");
-    //nasil olmus
-    printf("change is always good");
+    int x;
+
+    printf("Lütfen sayiyi girin: ");
+    scanf("%d", &x);
+    printf("\n birden girdiğiniz sayıya kadar olan sayıların toplamı %d dir", sayiTopla(x));
+
     return 0;
+}
+
+int sayiTopla(int x) {
+    if (x <= 0) {
+        return 0;
+    }
+
+    return x + sayiTopla(x - 1);
 }
