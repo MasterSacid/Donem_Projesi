439b3337c6acf79ddabaa8058d6e538d6b663e13
Akın <24360859032@ogr.btu.edu.tr>
2024-12-13 21:01:54 +0300

    Menüler iyileştirildi ve birkaç hata giderildi

--- include/menu.h
+++ include/menu.h
@@ -9,9 +9,10 @@ typedef struct menu{
     wchar_t menuItems[16][64];
     int itemCount;
     struct menu* children[16];
+    int childrenCount;
     struct menu* parent;
 }menu,*pmenu;
 
-void initMenu(pmenu menu,wchar_t name[64],wchar_t description[128],wchar_t menuItems[16][64],int itemCount,pmenu children[16],pmenu parent);
-void displayMenu(HANDLE stdOut,pmenu menu,int itemIndex,PCOORD coord);
+void initMenu(pmenu menu, wchar_t name[64], wchar_t description[128], wchar_t menuItems[][64], int itemCount, pmenu children[16], int childrenCount, pmenu parent);
+void displayMenu(HANDLE stdOut, pmenu menu, int itemIndex, PCOORD coord);
 #endif
--- main.c
+++ main.c
@@ -9,44 +9,27 @@
 // Ekran boyutunu saklamak için açılan COORD yapısı
 COORD coord;
 
-enum{
-    relocation,
-    food,
-    sleep,
-    camp
-};
-
 int main(void) {
     setlocale(LC_ALL, "");
     HANDLE stdOut=GetStdHandle(STD_OUTPUT_HANDLE);
     HANDLE stdIn=GetStdHandle(STD_INPUT_HANDLE);
 
     //Ana menü
-    menu main_menu,test;
-    initMenu(
-        &test,
-        L"Test Menü",
-        L"Desc",
-        (wchar_t[][64]){L"Evet",L"Hayır"},
-        2,
-        NULL,
-        &main_menu
-    );
-
+    menu main_menu;
     initMenu(
         &main_menu,
         L"Ana Menü",
         L"Ne yapmak istediğini seç",
         (wchar_t[][64]){L"Yola çık",L"Yemek ye",L"Uyu",L"Kamp kur"},
-        1,
-        (pmenu[]){&test},
+        4,
+        NULL,
+        0,
         NULL
     );
 
     CONSOLE_SCREEN_BUFFER_INFO bufferInfo;
 
     //Başlangıç Seçenekleri
-    pmenu menus[]={&test};
 
     pmenu selectedMenu=&main_menu;
     int itemIndex=0;
@@ -57,40 +40,46 @@ int main(void) {
         displayMenu(stdOut,selectedMenu,itemIndex,&coord);
         displayVertLine(stdOut,&coord,(COORD){33,0},(COORD){33,60},'|');
         displayHorLine(stdOut,&coord,(COORD){0,15},(COORD){33,15},'-');
+
         //Klavyeden geçerli tuş alınması
         int key=-1;
         while(key==-1){
             key=waitKeys(stdIn,(WORD[]){VK_UP,VK_DOWN,VK_RETURN},3);
         }
         //Klavyeden alınan tuşa göre işlem yapılması
+        int totalCount=selectedMenu->childrenCount+selectedMenu->itemCount;
         switch(key){
             case -1:
                 continue;
             case 0:
                 //Menü en yukarıdayken yukarı tuşuna basıldığında en aşağı inme
                 if(itemIndex<=0){
-                    itemIndex=selectedMenu->itemCount;
+                    itemIndex=totalCount;
                 }else{
                     itemIndex--;
                 }
                 break;
             case 1:
                 //Menü en aşağıdayken aşağı tuşuna basıldığında en yukarı çıkma
-                if(itemIndex>=selectedMenu->itemCount){
+                if(itemIndex>=totalCount){
                     itemIndex=0;
                 }else{
                     itemIndex++;
                 }
                 break;
             case 2:
-                if(itemIndex>selectedMenu->itemCount){
-                    return 0;
-                }else if(selectedMenu==&main_menu){
-                    selectedMenu=menus[itemIndex];
+                if(itemIndex>=totalCount){
+                    if(selectedMenu==&main_menu){
+                        return 0;
+                    }else{
+                        selectedMenu=selectedMenu->parent;
+                    }
+                }else if(itemIndex<selectedMenu->childrenCount){
+                    selectedMenu=selectedMenu->children[itemIndex];
                 }
                 break;
-        }
+            }
         clear(stdOut,&coord);
-    }
+        }
     return 0;
 }
