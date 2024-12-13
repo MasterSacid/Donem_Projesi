12153dbbe2ed0e3daa63738f9dfeef10f0279dbb
Akın <24360859032@ogr.btu.edu.tr>
2024-12-13 16:45:28 +0300

    Menu enumerate ve array eklendi

--- main.c
+++ main.c
@@ -9,26 +9,45 @@
 // Ekran boyutunu saklamak için açılan COORD yapısı
 COORD coord;
 
+enum{
+    relocation,
+    food,
+    sleep,
+    camp
+};
+
 int main(void) {
     setlocale(LC_ALL, "");
     HANDLE stdOut=GetStdHandle(STD_OUTPUT_HANDLE);
     HANDLE stdIn=GetStdHandle(STD_INPUT_HANDLE);
 
     //Ana menü
-    menu main_menu;
+    menu main_menu,test;
+    initMenu(
+        &test,
+        L"Test Menü",
+        L"Desc",
+        (wchar_t[][64]){L"Evet",L"Hayır"},
+        2,
+        NULL,
+        &main_menu
+    );
+
     initMenu(
         &main_menu,
         L"Ana Menü",
         L"Ne yapmak istediğini seç",
         (wchar_t[][64]){L"Yola çık",L"Yemek ye",L"Uyu",L"Kamp kur"},
-        4,
-        NULL,
+        1,
+        (pmenu[]){&test},
         NULL
     );
 
     CONSOLE_SCREEN_BUFFER_INFO bufferInfo;
 
     //Başlangıç Seçenekleri
+    pmenu menus[]={&test};
+
     pmenu selectedMenu=&main_menu;
     int itemIndex=0;
     clear(stdOut,&coord);
@@ -64,7 +83,11 @@ int main(void) {
                 }
                 break;
             case 2:
-                return 0;
+                if(itemIndex>selectedMenu->itemCount){
+                    return 0;
+                }else if(selectedMenu==&main_menu){
+                    selectedMenu=menus[itemIndex];
+                }
                 break;
         }
         clear(stdOut,&coord);
