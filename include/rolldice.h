ed5324e1b08d3f4bb9ab26b9fc3c33b8b66c161f
MasterSacid <sacidaktan1@hotmail.com>
2024-12-13 21:36:45 +0300

    Zar sistemi ve efektleri eklendi.

---
+++ include/rolldice.h
@@ -0,0 +1,11 @@
+#ifndef DICE_H
+#define DICE_H
+
+#include <windows.h>
+
+// Function Declarations
+void chooseDiceTextFile(int randomNumber);
+void clearScreen();
+int rollDice();
+
+#endif // DICE_H
