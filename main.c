a4ad5aad5bcf7f8322cf1c78a5424990ff4e253c
MasterSacid <sacidaktan1@hotmail.com>
2024-12-13 17:57:45 +0300

    Added menuChildren array. added some menus (couldnt link them) and changed them changed enums

--- main.c
+++ main.c
@@ -10,10 +10,10 @@
 COORD coord;
 
 enum{
-    relocation,
-    food,
-    sleep,
-    camp
+    startAdventure,
+    talkToSomeone,
+    sing,
+    eatFood
 };
 
 int main(void) {
@@ -22,31 +22,66 @@ int main(void) {
     HANDLE stdIn=GetStdHandle(STD_INPUT_HANDLE);
 
     //Ana menü
-    menu main_menu,test;
+    menu main_menu,startAdventure,eatFood,talkToSomeone,sing;
+    pmenu mainMenuChildren[50] = {&startAdventure, &eatFood, &talkToSomeone, &sing};
+
+
     initMenu(
-        &test,
-        L"Test Menü",
-        L"Desc",
-        (wchar_t[][64]){L"Evet",L"Hayır"},
-        2,
+      &startAdventure,
+      L"Maceraya Atıl",
+      L"Bu Maceraya atılmak istediğinden emin misin ?",
+      (wchar_t[][64]){L"Evet",L"Hayır"},
+      2,
+      NULL,
+      &main_menu
+  );
+
+    initMenu(
+      &talkToSomeone,
+      L"Biriyle konuş",
+      L"Kiminle konuşacaksın ?",
+      (wchar_t[][64]){L"Ayyaş",L"Barmen",L"Gezgin",L"Evsiz"}, //Bu kişileri seçtikten sonra yanda resimler olabilir
+      4,
+      NULL,
+      &main_menu
+  );
+
+    initMenu(
+       &sing,
+       L"Şarkı söyle",
+       L"İstdeğin şarkıyı seç",
+       (wchar_t[][64]){L"Şarkı1",L"Şarkı2"},
+       2,
+       NULL,
+       &main_menu
+   );
+    initMenu(
+        &eatFood,
+        L"Satın almak istediğin yemeği seç",
+        L"Hangi yemeği alacaksın",
+        (wchar_t[][64]){L"Yemek1",L"Yemek2",L"Yemek3",L"Yemek4",L"Yemek5"},
+        5,
         NULL,
         &main_menu
     );
+    // Önemli not : Yemek menüsü seçildikten sonra yanda ascii resimler gösterilebilir. Bu bir fikir sadece
+
+
 
     initMenu(
         &main_menu,
         L"Ana Menü",
         L"Ne yapmak istediğini seç",
-        (wchar_t[][64]){L"Yola çık",L"Yemek ye",L"Uyu",L"Kamp kur"},
-        1,
-        (pmenu[]){&test},
+        (wchar_t[][64]){L"Maceraya atıl",L"Etraftakilerle konuş",L"Şarkı söyle",L"Yemek ye"},
+        4,
+        mainMenuChildren,
         NULL
     );
 
     CONSOLE_SCREEN_BUFFER_INFO bufferInfo;
 
     //Başlangıç Seçenekleri
-    pmenu menus[]={&test};
+    pmenu menus[]={&startAdventure,&talkToSomeone,&sing,&eatFood};
 
     pmenu selectedMenu=&main_menu;
     int itemIndex=0;
